347fdbc9226087d91839850204e37322a2d83cfb
BohdanMus <bogdanmusienko@gmail.com>
2023-02-16 18:00:50 +0200

    finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,3 @@
-
 // Напиши скрипт управління формою логіна.
 
 // <form class="login-form">
@@ -18,3 +17,21 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", btnSubmit);
+function btnSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Заповни всі поля!");
+  }
+  const userInfo = {
+    email: email.value,
+    password: password.value
+  };
+  console.log(userInfo);
+  event.currentTarget.reset();
+}
